e55bb993e3dfa5347ba4829a799c88c4e700e836
jasmine <jazyan11@gmail.com>
2017-09-20 00:22:19 -0700

    flatten tree works, use make_heap instead of priority_queue

--- huffman/huffman.cpp
+++ huffman/huffman.cpp
@@ -1,8 +1,8 @@
 #include <fstream>
-#include <queue>
 #include <iostream>
 #include <assert.h>
 #include <string>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
@@ -10,69 +10,64 @@ using namespace std;
 
 class node {
   public:
-    bool operator<(const node& other) const {
-        return freq > other.freq;  // for q.pop to return lowest frequency
-    }
     char value = ' ';
     int freq = 0;
     node* left = NULL;
     node* right = NULL;
 };
 
-void print_queue(priority_queue<node> q) {
-    while (!q.empty()) {
-        node n = q.top();
-        cout << n.freq<< " " << n.value << endl;
-        q.pop();
+struct node_f{
+    bool operator()(const node* a, const node* b) const {
+        return a->freq > b->freq;
     }
-}
+};
+
+// TODO: need to delete tree (traverse tree and destroy)
 
 // pop off two lowest frequencies and combine them into one internal node I
 // I's left child is smaller of two, right child is larger
 // continue until there is only one node, which is the root. return that
-node huffman_tree(priority_queue<node>& q) {
+node* huffman_tree(vector<node*> q) {
     assert(q.size());  // TODO: asserts are only in debug mode?
     if (q.size() == 1) {
-        node n = q.top();
-        q.pop();
+        pop_heap(q.begin(), q.end()); 
+        node* n = q.back(); 
+        q.pop_back();
         return n;
     }
-    node n1 = q.top(); q.pop();
-    node n2 = q.top(); q.pop();
-    cout << n1.freq << " " << n2.freq << endl;
-    node parent_n;
-    parent_n.freq = n1.freq + n2.freq;
-    parent_n.value = ' ';
-    if (n1.freq < n2.freq) {
-        parent_n.left = &n1;
-        parent_n.right = &n2;
+    pop_heap(q.begin(), q.end(), node_f()); 
+    node *n1 = q.back(); q.pop_back();
+    pop_heap(q.begin(), q.end(), node_f());
+    node *n2 = q.back(); q.pop_back();
+    cout << n1->freq << " " << n2->freq << endl;
+    node *parent_n = new node;
+    parent_n->freq = n1->freq + n2->freq;
+    parent_n->value = ' ';
+    if (n1->freq < n2->freq) {
+        parent_n->left = n1;
+        parent_n->right = n2;
     } else {
-        parent_n.left = &n2;
-        parent_n.right = &n1;
+        parent_n->left = n2;
+        parent_n->right = n1;
     } // TODO: add case of value ties? 
     // but then have to define value of internal nodes
-    q.push(parent_n);
+    q.push_back(parent_n); push_heap(q.begin(), q.end(), node_f());
     return huffman_tree(q);
 }
 
-// left node should be 0, right node should be 1 
+// left node should be 0, right node should be 1
 void flatten_tree(node* n, int val, vector<int>& res) {    
     if (n->left == NULL && n->right == NULL) {
         int ind = static_cast<int>(n->value);
-        cout << "char to ind" << ind << endl;
+        cout << "char to ind " << ind << " " << val << endl;
         assert(ind < 128);
         res[ind] = val; 
-        cout << "success?" << endl; 
         return;
     }
     if (n->left != NULL) {
-        assert(n->value != 'H');
-        cout << "fff" << endl;
         flatten_tree(n->left, val << 1, res);
     }
     if (n->right != NULL) {
-        assert(n->value == ' ' && n->value && "right");
-        cout << "rrr" << endl;
         flatten_tree(n->right, (val << 1) + 1, res);
     }
 }
@@ -80,19 +75,19 @@ void flatten_tree(node* n, int val, vector<int>& res) {
 int main () {
     ifstream infile("freq.txt");
     char a; int b;
-    priority_queue<node> q;
+    vector<node*> node_heap;
     while (infile >> a >> b) {
-        node n;
-        n.value = a;
-        n.freq = b;
-        n.left = NULL;
-        n.right = NULL;
-        q.push(n);
+        node *n = new node;
+        n->value = a;
+        n->freq = b;
+        n->left = NULL;
+        n->right = NULL;
+        node_heap.push_back(n);
     }
-    node root = huffman_tree(q);
-    vector<int> dict(128, 0);
-    flatten_tree(&root, 0, dict);
+    make_heap(node_heap.begin(), node_heap.end(), node_f());
 
-    //cout << dict['a'] << endl; 
+    node *root = huffman_tree(node_heap);
+    vector<int> dict(128, 0);
+    flatten_tree(root, 0, dict);
     return 0;
 }
