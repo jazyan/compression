9f352ee07fe2795e2ab4eb601d1ad5ef6e3a5530
Jasmine Yan <jazyan11@gmail.com>
2017-09-19 18:18:18 -0700

    no more friends, start of traverse / flatten tree

--- huffman/huffman.cpp
+++ huffman/huffman.cpp
@@ -2,19 +2,20 @@
 #include <queue>
 #include <iostream>
 #include <assert.h>
+#include <string>
 using namespace std;
 
 // http://www.geeksforgeeks.org/greedy-algorithms-set-3-huffman-coding/
 
 class node {
   public:
-    friend bool operator<(const node &n1, const node &n2) {
-        return n1.freq > n2.freq;  // for q.pop to return lowest frequency
+    bool operator<(const node& other) const {
+        return freq > other.freq;  // for q.pop to return lowest frequency
     }
     char value;
     int freq;
-    node* left;
-    node* right;
+    node* left = NULL;
+    node* right = NULL;
 };
 
 void print_queue(priority_queue<node> q) {
@@ -28,16 +29,16 @@ void print_queue(priority_queue<node> q) {
 // pop off two lowest frequencies and combine them into one internal node I
 // I's left child is smaller of two, right child is larger
 // continue until there is only one node, which is the root. return that
-node huffman_tree(priority_queue<node> q, int n) {
-    assert(n);
-    if (n == 1) {
+node huffman_tree(priority_queue<node>& q) {
+    assert(q.size());  // TODO: asserts are only in debug mode?
+    if (q.size() == 1) {
         node n = q.top();
         q.pop();
         return n;
     }
     node n1 = q.top(); q.pop();
     node n2 = q.top(); q.pop();
-    cout << n1.freq << " " << n2.freq << " " << n << endl;
+    cout << n1.freq << " " << n2.freq << endl;
     node parent_n;
     parent_n.freq = n1.freq + n2.freq;
     parent_n.value = ' ';
@@ -50,24 +51,32 @@ node huffman_tree(priority_queue<node> q, int n) {
     } // TODO: add case of value ties? 
     // but then have to define value of internal nodes
     q.push(parent_n);
-    return huffman_tree(q, --n);
+    return huffman_tree(q);
 }
+
+// left node should be 0, right node should be 1 
+// TODO: why is it that this guarantees unique prefixes?
+// because all the nodes are leaves? are they all leaves? --> yes, they have no children lol
+void traverse_tree(node& n, string& s) {
+    ofstream outfile("translate.txt");
+    if (n.left == NULL && n.right == NULL) {
+        outfile << n.value << " " << s << endl;
+        return;
+    }
+    // TODO: recurse
+}
+
 
 int main () {
     ifstream infile("freq.txt");
-    char a; int b, num = 0;
+    char a; int b;
     priority_queue<node> q;
     while (infile >> a >> b) {
         node n;
         n.value = a;
         n.freq = b;
         q.push(n);
-        num++;
     }
-    // print_queue(q);
-    if (num <= 1) {
-        return 0;
-    }
-    node root = huffman_tree(q, num);
+    node root = huffman_tree(q);
     return 0;
 }
