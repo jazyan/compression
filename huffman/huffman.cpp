538b5af19467af5b02f76e5e906503f5307ca004
jasmine <jazyan11@gmail.com>
2017-10-15 18:19:20 -0700

    add encode portion (untested)

--- huffman/huffman.cpp
+++ huffman/huffman.cpp
@@ -5,6 +5,7 @@
 #include <unordered_map>
 #include <string>
 #include <vector>
+
 using namespace std;
 
 // http://www.geeksforgeeks.org/greedy-algorithms-set-3-huffman-coding/
@@ -17,10 +18,15 @@ class node {
     node* right = NULL;
 };
 
-struct node_f{
+struct node_f {
     bool operator()(const node* a, const node* b) const {
         return a->freq > b->freq;
     }
+};
+
+struct dictval {
+    int int_value;
+    int length;
 };
 
 void destroy_tree(node* n) {
@@ -35,7 +41,6 @@ void destroy_tree(node* n) {
 // I's left child is smaller of two, right child is larger
 // continue until there is only one node, which is the root. return that
 node* huffman_tree(vector<node*> q) {
-    assert(q.size());  // TODO: asserts are only in debug mode?
     if (q.size() == 1) {
         pop_heap(q.begin(), q.end()); 
         node* n = q.back(); 
@@ -46,7 +51,6 @@ node* huffman_tree(vector<node*> q) {
     node *n1 = q.back(); q.pop_back();
     pop_heap(q.begin(), q.end(), node_f());
     node *n2 = q.back(); q.pop_back();
-    // cout << n1->freq << " " << n2->freq << endl;
     node *parent_n = new node;
     parent_n->freq = n1->freq + n2->freq;
     parent_n->value = 128;  // outside ASCII range
@@ -63,18 +67,61 @@ node* huffman_tree(vector<node*> q) {
 }
 
 // left node should be 0, right node should be 1
-void flatten_tree(node* n, int val, vector<int>& res) {    
+void flatten_tree(node* n, int val, int length, vector<dictval>& res) {    
     if (n->left == NULL && n->right == NULL) {
         cout << static_cast<char>(n->value) << " " << val << endl;
-        assert(n->value < 128);
-        res[n->value] = val; 
+        res[n->value].int_value = val;
+        res[n->value].length = length;
         return;
     }
     if (n->left != NULL) {
-        flatten_tree(n->left, val << 1, res);
+        flatten_tree(n->left, val << 1, length + 1, res);
     }
     if (n->right != NULL) {
-        flatten_tree(n->right, (val << 1) + 1, res);
+        flatten_tree(n->right, (val << 1) + 1, length + 1, res);
+    }
+}
+
+
+void encode (vector<dictval>& dict) {
+    ifstream infile("orig.txt");
+    FILE* outfile = fopen("compress.bin", "wb");
+    uint64_t towrite = 0;
+    int curr_bits = 0;
+    char c;
+    while (infile.get(c)) {
+        dictval d = dict[static_cast<int>(c)];
+        int val = d.int_value;
+        int l = d.length;
+        // write each bit of value to the uint64_t
+        // increment the number of bits used, and
+        // write to file if number of bits reaches 64
+        while (val != 0) {
+            int bit = val & 1;
+            if (bit == 1) {
+                towrite |= 1 << (64 - curr_bits);
+            }
+            val >>= 1;
+            curr_bits++;
+            l--;
+            if (curr_bits == 64) {
+                fwrite(&towrite, sizeof(uint64_t), 1, outfile);
+                towrite = 0;
+                curr_bits = 0;
+            }
+        }
+        // leading 0s would get lost with just int representation
+        // "write" the remaining zeros by incrementing curr_bits
+        // and writing when exceeding 64 bits
+        for (int i = 1; i <= l; i++) {
+            curr_bits++;
+            if (curr_bits == 64) {
+                fwrite(&towrite, sizeof(uint64_t), 1, outfile);
+                towrite = 0;
+                curr_bits = 0;
+            }
+        }
+        // cout << "what is towrite? " << towrite << endl;
     }
 }
 
@@ -84,10 +131,6 @@ int main () {
     unordered_map<int, int> char_freq_map;
     while (infile.get(c)) {
         int key = static_cast<int>(c);
-        // TODO: including this seems to break it?
-        // if (key < 0 || key > 127) {  // filter out non-ascii
-        //     continue;
-        // }
         char_freq_map[key]++;
     }
     vector<node*> node_heap;
@@ -100,16 +143,12 @@ int main () {
         node_heap.push_back(n);
     }
     make_heap(node_heap.begin(), node_heap.end(), node_f());
-
     node *root = huffman_tree(node_heap);
-    vector<int> dict(128, 0);
-    flatten_tree(root, 0, dict);
-    
-    // TODO:
-    // read in file again, but for each ASCII character, pack it into an
-    // int64 (left shift current number by num of bits, check if < 64 bits,
-    // if so add number, else fwrite current number and start new number)
-    
+   
+    dictval base = {0, 0};
+    vector<dictval> dict(128, base);
+    flatten_tree(root, 0, 0, dict);
+    encode (dict);
     // TODO: decode it back and check if contents are the same
     
     // clean up
